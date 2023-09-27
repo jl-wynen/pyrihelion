e96cfd6dd9779a883863ffef025803a9a995a1d0
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-09-27 10:21:57 +0200

    Run pre-commit

--- src/App.vue
+++ src/App.vue
@@ -10,33 +10,33 @@ const editor = ref<Editor | null>(null)
 const textOutput = ref<TextOutput | null>(null)
 
 function setCode() {
-  textOutput.value.setContent(editor.value.getCode())
+    textOutput.value.setContent(editor.value.getCode())
 }
 
 import SplitPane from "./components/SplitPane.vue"
 </script>
 
 <template>
-  <div id="app-main">
-    <ToolBar/>
-    <!-- prettier-ignore-attribute -->
-    <SplitPane direction="horizontal" initial_fraction="0.5">
-      <template v-slot:first>
+    <div id="app-main">
+        <ToolBar />
         <!-- prettier-ignore-attribute -->
-        <SplitPane direction="vertical" initial_fraction="0.8">
-          <template v-slot:first>
-            <Editor ref="editor" />
-          </template>
-          <template v-slot:second>
-            <TextOutput ref="textOutput" />
-          </template>
+        <SplitPane direction="horizontal" initial_fraction="0.5">
+            <template v-slot:first>
+                <!-- prettier-ignore-attribute -->
+                <SplitPane direction="vertical" initial_fraction="0.8">
+                    <template v-slot:first>
+                        <Editor ref="editor" />
+                    </template>
+                    <template v-slot:second>
+                        <TextOutput ref="textOutput" />
+                    </template>
+                </SplitPane>
+            </template>
+            <template v-slot:second>
+                <Canvas ref="canvas" />
+            </template>
         </SplitPane>
-      </template>
-      <template v-slot:second>
-        <Canvas ref="canvas" />
-      </template>
-    </SplitPane>
-  </div>
+    </div>
 </template>
 
 <style scoped></style>
