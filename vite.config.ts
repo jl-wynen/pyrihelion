36a2ef5537b0fa92058f349de8fc04ab36060f8d
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-10-30 10:18:39 +0100

    Use plugin also for worker

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import { pyPackagePlugin } from "./setup/vite-plugin-py-package.js"
 
+const gangleriPlugin = pyPackagePlugin("gangleri", "src-py/gangleri")
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [pyPackagePlugin("gangleri", "src-py/gangleri"), vue()],
+    plugins: [gangleriPlugin, vue()],
     base: "/pyrihelion",
     worker: {
         format: "es",
+        plugins: [gangleriPlugin],
     },
 })
