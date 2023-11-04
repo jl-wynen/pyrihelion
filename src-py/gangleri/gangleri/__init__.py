fb7487c087d30e497faf7a8569e6b886c109726a
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-11-04 19:32:52 +0100

    Provide gangleri.__version__

--- src-py/gangleri/gangleri/__init__.py
+++ src-py/gangleri/gangleri/__init__.py
@@ -1,5 +1,12 @@
 """High-level wrappers for the Gangleri engine."""
 
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
 from ._engine import clear, init
 from ._entity import Box, Sphere
 from ._vector import Vector3
